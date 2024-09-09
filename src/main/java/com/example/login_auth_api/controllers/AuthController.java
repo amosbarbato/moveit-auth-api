2ccbdd0cf50cfacaef0904201b5d3baae41d288b
Amos Barbato <amosbarbato@gmail.com>
2024-09-09 15:49:05 -0300

    feat: create authentic controller

---
+++ src/main/java/com/example/login_auth_api/controllers/AuthController.java
@@ -0,0 +1,50 @@
+package com.example.login_auth_api.controllers;
+
+import com.example.login_auth_api.domain.user.User;
+import com.example.login_auth_api.dto.LoginRequestDTO;
+import com.example.login_auth_api.dto.RegisterRequestDTO;
+import com.example.login_auth_api.dto.ResponseDTO;
+import com.example.login_auth_api.infra.security.TokenService;
+import com.example.login_auth_api.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PutMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body){
+        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+        if(passwordEncoder.matches(body.password(), user.getPassword())) {
+            String token = this.tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDTO(user.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body) {
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if(user.isEmpty()) {
+            User newUser = new User();
+            newUser.setName(body.name());
+            newUser.setEmail(body.email());
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            this.repository.save(newUser);
+
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(newUser.getName(), token));
+        }
+        return ResponseEntity.badRequest().build();
+    }
+}
---
+++ src/main/java/com/example/login_auth_api/dto/LoginRequestDTO.java
@@ -0,0 +1,4 @@
+package com.example.login_auth_api.dto;
+
+public record LoginRequestDTO(String email, String password) {
+}
