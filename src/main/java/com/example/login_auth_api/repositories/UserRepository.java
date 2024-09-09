c461e5b515f6d26aaa0c272c9be159c450be7f4f
Amos Barbato <amosbarbato@gmail.com>
2024-09-09 15:03:39 -0300

    feat: create security filter

--- src/main/java/com/example/login_auth_api/repositories/UserRepository.java
+++ src/main/java/com/example/login_auth_api/repositories/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.login_auth_api.repositories;
 import com.example.login_auth_api.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, String> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
